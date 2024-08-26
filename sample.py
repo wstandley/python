1422a4d95fd4aa753b8993c42669463b5f882c3d
wstandley <122952912+wstandley@users.noreply.github.com>
2024-08-26 14:49:08 -0500

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,20 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 5)
+
+# Integer
+y = -365
+print("Integer:", y)
+
+# Float
+z = 3.14
+print("Float:", z)
+
+# String
+name = "Will"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
