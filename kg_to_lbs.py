c85a105f91bc1487b2a53b66bc2f504ae7dbb3c7
wstandley <122952912+wstandley@users.noreply.github.com>
2024-08-28 14:48:49 -0500

    Add files via upload

---
+++ kg_to_lbs.py
@@ -0,0 +1,22 @@
+# Assignment PE1 2.4: Python Variables and Formatting Numbers
+
+# Variables for kilograms
+kg_value = 6
+kg_value2 = 14
+kg_value3 = 38
+kg_value4 = 95
+
+# Conversion lb = kg * 2.20462
+lb_to_kg = 2.20462
+
+# Calculations
+pounds = kg_value * lb_to_kg
+pounds2 = kg_value2 * lb_to_kg
+pounds3 = kg_value3 * lb_to_kg
+pounds4 = kg_value4 * lb_to_kg
+
+# Output Statements
+print(f"{kg_value} kilograms is equal to {pounds: .2f} pounds.")
+print(f"{kg_value2} kilograms is equal to {pounds2: .2f} pounds.")
+print(f"{kg_value3} kilograms is equal to {pounds3: .2f} pounds.")
+print(f"{kg_value4} kilograms is equal to {pounds4: .2f} pounds.")
