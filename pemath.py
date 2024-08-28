1feeb843c2f70e07cdeee07a867ffb697c03f1f3
wstandley <122952912+wstandley@users.noreply.github.com>
2024-08-28 14:02:31 -0500

    Add files via upload

---
+++ pemath.py
@@ -0,0 +1,9 @@
+# Assignment for PE1 2.3: Mathmatical Operations
+
+# Variables
+length = 12
+width = 4
+area = length * width # Area Equation
+
+# Print Statement
+print("The area of the rectangle with a length of" , length , "and a width of" , width , "is" , area , ".")
