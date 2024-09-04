7be2a129f659285d26d57a2381b19b434ed625ed
wstandley <122952912+wstandley@users.noreply.github.com>
2024-09-04 13:34:22 -0500

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,16 @@
+# This is for assignment PE1 2.5: Python Comments
+
+tank_size = 15 #This is consider an integer variable
+distance = 600
+mpg = distance / tank_size # This is division in Python
+
+"""
+    The above lines 3-5 are considered variables
+"""
+
+print("Miles per gallon:", mpg)
+
+"""
+    Type the above code into Visual Studio Code
+    Add a minimum of 4 comments to the above Python program to make it more understandable.
+"""
