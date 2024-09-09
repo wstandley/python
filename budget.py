6e40450def495c724739fa630d93293b6d0fdd85
wstandley <williamstandley03@gmail.com>
2024-09-09 14:56:25 -0500

    Assignment PE1 2.6

--- budget.py
+++ budget.py
@@ -1,3 +1,19 @@
 """
     This is for assignemnt PE1 2.6: Interacting with the User: Input
+    Thsi helps calculate a monthly budget.
 """
+
+# Variables
+
+
+
+# Floats
+
+housing = float("How much do you pay for your housing? ")
+utilities = float("How much is your typical utility bill? ")
+groceries =
+transportation =
+health_care = 
+personal_care =
+clothing =
+debt = 
