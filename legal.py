a2ae3eec3492651e56eea17edf7e413d202449e1
wstandley <williamstandley03@gmail.com>
2024-09-16 16:19:44 -0500

    Assignment PE1 3.1

--- legal.py
+++ legal.py
@@ -1,3 +1,24 @@
 """
-    This is for assignment PE1 3.1: Evaluating Expressions and Making Decisions in Python
+    This is for assignment PE1 3.1: Making Decisions in Python
 """
+
+# Variables
+age = int(input("How old are you? "))
+
+# Decisions
+if age < 16:
+    print("You are not old enough to drive.")
+else:
+    print("You are old enough to drive.")
+if age < 18:
+    print("You are not old enough to vote.")
+else:
+    print("You are old enough to vote.")
+if age < 21:
+    print("You are not old enough to buy alcohol.")
+else:
+    print("You are old enough to purchase alcohol.")
+if age < 65:
+    print("You are not eligible for a senior discount.")
+else:
+    print("You are eligible for a senior discount.")
