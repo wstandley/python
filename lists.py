637be660d40787752a63d26a84f4e4e1fc5db7d6
wstandley <williamstandley03@gmail.com>
2024-09-25 14:35:28 -0500

    Assignment PE1 3.4

--- lists.py
+++ lists.py
@@ -7,12 +7,13 @@
 days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 steps = []
 total_steps = 0
+total = 0
 average = 0
 
 # User Input
 
 for i in range(len(days)):
-    day = days[i]
+    day = days[i] # days[i] allows for each item in list to show
     steps_taken = int(input(f"How many steps did you take on {day}? "))# Prints input for each day
     steps.append(steps_taken) # Append (Has to be in the for statement line of code)
     total_steps += steps_taken # Only works because of Integer Input
@@ -21,12 +22,14 @@ for i in range(len(days)):
 # Display of Total Steps Per Day
 
 for i in range(len(steps)):
+    day = days[i] 
     step = steps[i]
+    total += int(step) # Converts string to integer
     print(f"You took {step} steps on {day}. ")
 
 # Display Total Steps for Week
 print(f"\nWeek Total Steps: {total_steps}")
 
 # Display Average steps for the week
-average = average / steps
-print(f"Average Daily Steps: {average}")
+average = round(total/ len(steps))
+print(f"Average Daily Steps: {average: .0f}")
