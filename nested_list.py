1d3fb98b647942698364497b4d3b86f8826a4b39
wstandley <williamstandley03@gmail.com>
2024-09-29 15:42:07 -0500

    Assignment: PE1 3.7

---
+++ nested_list.py
@@ -0,0 +1,21 @@
+"""
+    This is for Assignment PE1 3.7: Lists in Advanced applications | Arrays
+"""
+
+# Variables
+total = 0
+
+# List Initializations
+
+time_slots = ["Morning", "Midday", "Afternoon", "Evening"]
+heart_rates = []
+
+# Input
+for time in range(len(time_slots)):
+    time = time_slots[time]
+    rates = int(input(f"Please enter your heart rate level during the {time} (in nearest whole number): "))
+    heart_rates.append([time,rates])
+    total += int(rates)
+
+average = round(total/len(heart_rates))
+print(f"\nYour average heart rate for today was: {average}")
