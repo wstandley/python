1dc8131af98b5e6f0563fb7b1164450b7a4798c2
wstandley <williamstandley03@gmail.com>
2024-09-29 15:43:23 -0500

    Assignment PE1 3.7

--- nested_list.py
+++ nested_list.py
@@ -14,8 +14,8 @@ heart_rates = []
 for time in range(len(time_slots)):
     time = time_slots[time]
     rates = int(input(f"Please enter your heart rate level during the {time} (in nearest whole number): "))
-    heart_rates.append([time,rates])
-    total += int(rates)
+    heart_rates.append([time,rates]) # Allows for input to be added for specific slot
+    total += int(rates) # Adds inputs to total variable 
 
 average = round(total/len(heart_rates))
 print(f"\nYour average heart rate for today was: {average}")
