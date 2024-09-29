46d58cce563c03e437f0c571b73ff32656e71158
wstandley <williamstandley03@gmail.com>
2024-09-29 12:58:49 -0500

    Assignment: Bubble Sort

--- bubble.py
+++ bubble.py
@@ -1,3 +1,25 @@
 """
     This is for assignment PE1 3.5: Sorting Simple Lists: Bubble Sort
 """
+
+# list initialization
+number = ["first","second","third","fourth","fifth"]
+names = []
+
+for i in range(len(number)):
+    numbers = number[i]
+    name = input(f"What is the {numbers} name? ")
+    names.append(name)
+
+swapped = True
+
+while swapped:
+    swapped = False
+    for i in range(len(names)-1):
+        if names[i] > names[i + 1]:
+            swapped = True
+            names[i], names [i + 1] = names [i + 1], names[i]
+
+print(names)
+names.reverse()
+print(names)
