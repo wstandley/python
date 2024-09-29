d130ef9356cee72ecec89e5db64ba4fbdae92db8
wstandley <williamstandley03@gmail.com>
2024-09-29 13:05:15 -0500

    Assignment: Bubble Sort

--- bubble.py
+++ bubble.py
@@ -11,15 +11,16 @@ for i in range(len(number)):
     name = input(f"What is the {numbers} name? ")
     names.append(name)
 
+# Bubble Sort Time
 swapped = True
 
 while swapped:
     swapped = False
-    for i in range(len(names)-1):
-        if names[i] > names[i + 1]:
+    for i in range(len(names)-1): # Goes through each item of the list
+        if names[i] > names[i + 1]: 
             swapped = True
-            names[i], names [i + 1] = names [i + 1], names[i]
+            names[i], names [i + 1] = names [i + 1], names[i] # Once they equal it ends the bubble sort
 
 print(names)
-names.reverse()
+names.reverse() # Reverses List
 print(names)
