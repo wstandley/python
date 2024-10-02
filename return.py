118b8e6151b4ebdddc0e56eeacd961f1afec674a
wstandley <williamstandley03@gmail.com>
2024-10-02 13:48:14 -0500

    Assignment PE1 4.3

--- return.py
+++ return.py
@@ -3,14 +3,15 @@
     Writing a Python Function named calculate_interest that computes and returns the simple interest based on given parameters
 """
 
-def calculate_interest(): # Defines the function
-    # Has to be float
-    principal = float(input("What is the principle amount? "))
-    rate = float(input("What is the interest rate? "))
-    time = float(input("How many years is the money going to be borrowed or invested? "))
-    # Simple Interest Formula
-    total = (principal * rate * time) / 100
-    return total
+# Has to be float
+principal = float(input("What is the principle amount? "))
+rate = float(input("What is the interest rate? "))
+time = float(input("How many years is the money going to be borrowed or invested? "))
 
-total = calculate_interest() # Calls the function
-print(f"The interest accrued will be {total}.")
+def calculate_interest(): # Defines the function
+    # Simple Interest Formula
+    calculated_interest = (principal * rate * time) / 100
+    return calculated_interest
+
+calculated_interest = calculate_interest() # Calls the function
+print(f"The simple interest for a principal amount of ${principal:,.2f}\nAt an interest rate of {rate}%\nOver a period of {time} years is : ${calculated_interest:,.2f}.")
