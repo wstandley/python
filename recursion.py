5f42f0ffffea89f05581ea8bd7c9ce2888cfe0f7
wstandley <williamstandley03@gmail.com>
2024-10-07 20:04:52 -0500

    Assignment PE1 4.5

--- recursion.py
+++ recursion.py
@@ -3,17 +3,18 @@
     Creating a program to calculate factorials
 """
 
-def factorial(n):
+def factorial(n, num=1):
+    # Function to calculate the factorial
     if n == 0 or n == 1:
-        return print("The factorial is: 1")
+        return num
     else:
-        print(f"The factorial is: {n}")
-        return factorial(n - 1)
+        return factorial(n-1,num * n)
     
 def main():
+    # Main Function
     n = int(input("Enter in the number you'd like to get the factorial of (non-negative): "))
     fact = factorial(n)
     print(f"The factorial of {n} is: {fact}")
 
+# Calls Function main()
 main()
-
