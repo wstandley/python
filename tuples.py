0b1896d55b213cddf5a1e80b9c3646ba15aa9921
wstandley <williamstandley03@gmail.com>
2024-10-09 13:08:09 -0500

    Assignment PE1 4.6A

---
+++ tuples.py
@@ -0,0 +1,13 @@
+"""
+    This is for Assignment PE1 4.6A: Tuples
+"""
+
+def main():
+    # Enter Comment Here
+    programming_classes = ('Intro to Python', 'Advanced Python', 'Database Essentials', 'Web Development Basics', 'Structures in Python', 'Web Design Fundamentals')
+    for classes in programming_classes:
+        print(classes)
+        length = len(programming_classes)
+    print(f"\nThere are {length} classes for programming.")
+
+main()
