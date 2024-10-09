78555202f5172124a56fb2015032a4756021266e
wstandley <williamstandley03@gmail.com>
2024-10-09 13:08:55 -0500

    Assignment PE1 4.6A

--- tuples.py
+++ tuples.py
@@ -3,7 +3,7 @@
 """
 
 def main():
-    # Enter Comment Here
+    # This Program is for listing out what classes there are to choose from and counting how many there are
     programming_classes = ('Intro to Python', 'Advanced Python', 'Database Essentials', 'Web Development Basics', 'Structures in Python', 'Web Design Fundamentals')
     for classes in programming_classes:
         print(classes)
