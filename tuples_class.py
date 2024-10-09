4c370c2dcd55e773556dadf138b6fd03d0b6eb8d
wstandley <williamstandley03@gmail.com>
2024-10-09 13:00:46 -0500

    Class Example

---
+++ tuples_class.py
@@ -0,0 +1,12 @@
+'''
+    Class Example for tuples
+    Tuples are permanent and cannot be changed
+'''
+
+def main():
+    my_tuple = (1, 2, 3)
+    for item in my_tuple:
+        print(item)
+    print("The tuple has", len(my_tuple), "items.")
+
+main()
