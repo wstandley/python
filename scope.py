6a9673cb01b8baa24dadaad2b51e74e047106e08
wstandley <williamstandley03@gmail.com>
2024-10-14 14:06:59 -0500

    Assignment 4.4

--- scope.py
+++ scope.py
@@ -9,29 +9,32 @@ KILOS = 0.453592
 global METERS
 METERS = 0.0254
 
+def calculate_weight():
+    # Function to calculate the users weight into kilos
+    weight_lbs = float(input("What is your current weight in pounds? "))
+    weight = weight_lbs * KILOS
+    return weight
+
+def calculate_height():
+    # Function to calculate the users height into meters
+    height_in = float(input("What is your current height in inches? "))
+    height = height_in * METERS
+    return height
+
+
 # main function
 def main():
-    # user input
-    weight_lbs = float(input("What is your current weight in pounds? "))
-    height_in = float(input("What is your current height in inches? "))
-    # calculations based off input
-    weight = weight_lbs * KILOS
-    height = height_in * METERS
-    bmi = weight / (height * height)
-    # function works
-    return bmi
+    bmi = calculate_weight() / (calculate_height() ** 2)
+    # Print Statements
+    print(f"Your BMI is: {bmi:.2f}")
+    # Takes bmi and prints statement depending on outcome from main function
+    if bmi < 18.5:
+        print("You are in the underweight caategory.")
+    elif bmi < 24.9:
+        print("You are in the normal weight category.")
+    elif bmi < 29.9:
+        print("You are in the overweight category.")
+    else:
+        print("You are in the obese category.")
 
-bmi = main() # Makes bmi the variable for output of main function
-
-# Print Statements
-print(f"Your BMI is: {bmi:.2f}")
-
-# Takes bmi and prints statement depending on outcome from main function
-if bmi < 18.5:
-    print("You are in the underweight caategory.")
-elif bmi < 24.9:
-    print("You are in the normal weight category.")
-elif bmi < 29.9:
-    print("You are in the overweight category.")
-else:
-    print("You are in the obese category.")
+main() # Calls main function
