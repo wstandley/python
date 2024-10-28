61a1ac2bd7210cba58c112e69e4664fd1fbb05e8
wstandley <williamstandley03@gmail.com>
2024-10-27 20:07:34 -0500

    Assignment Modules

--- modules.py
+++ modules.py
@@ -1,5 +1,7 @@
 '''
     This is for Assignment PE2 Module 1.1 and 1.2- Modules
+
+    Can't figure out why it wont stop program once correctly guessed
 '''
 
 import random # imports the random 
@@ -7,21 +9,29 @@ correct = random.randint(1, 100)
 
 def main():
     # Main Function
-    try: # User Enters a number to guess
-        guess = int(input("Guess a number between 1 and 100: "))
-        abs(guess)
+    try: 
+       # User Enters a number to guess
+       guess = int(input("Guess a number between 1 and 100: "))
     # Excepts is user doesnt enter a whole number
     except ValueError:
         print("You entered an incorrect digit.")
+    # Calculate the Absolute Value of to get the difference range
+    difference = abs(guess - correct)
+    # if statement to compare how close it is to answer
+    if difference == 0:
+        print("You guessed it!")
+        print("All Done!")
+    elif difference <= 5:
+        print("Very Hot!")
+        main()
+    elif difference <= 15:
+        print("Hot!")
+        main()
+    elif difference <= 25:
+        print("Cold")
         main()
     else:
-        while guess != correct:
-            print("You guess wrong.")
-            main()
-    finally:
-        print("You guess correctly!")
-
-        
-
-
+        print("Very Cold")
+        main()
+             
 main()
