8a4c9ad217da010bc4448eb1d642fdeaff6d7a15
wstandley <williamstandley03@gmail.com>
2024-11-06 14:26:19 -0600

    Class Example

--- list_exceptions.py
+++ list_exceptions.py
@@ -4,9 +4,22 @@
 
 def main():
     top_artists = ["The Beatles", "Madonna", "Elton John", "Elvis Presley", "Mariah Carey", "Stevie Wonder", "Janet Jackson", "Michael Jackson", "Whitney Houston", "Rihanna"]
-    # Your code goes here
+    
     try:
-        artist_name = int(input("Enter the index of the artist to replace: "))
+        # Input for what artist top spot to take
+        artist_index = int(input("Enter the index of the artist to replace: "))
+
+        # Input for what artist to add to list
+        artist_name = input("Enter the new artist name: ")
+
+        for name in top_artists:
+            name = (artist_index - 1)
+            # Remove whatever artist is in the index spot
+            top_artists.remove(name)
+            # Append artist name to the list
+            top_artists.append(artist_name)
+
+        print(f"Updated List: {top_artists}")
     except (ValueError, IndexError) as e:
         print(f"An error occurred: {e}")
     
