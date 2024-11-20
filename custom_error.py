499c8686bf0410d904f42db143f136152db52ae8
wstandley <williamstandley03@gmail.com>
2024-11-20 14:14:32 -0600

    Test

--- custom_error.py
+++ custom_error.py
@@ -14,20 +14,19 @@ class NotNumericError(Exception):
     def __str__(self):
         return f"{self.number} -> {self.message}"
     
-def isnumeric(number):
-    # This is Where I'M stuck
-    if number :
-        raise NotNumericError(number)
-    print("Thanks!")
+    def isnumeric(number):
+        if number == False:
+            raise NotNumericError(number)
+        print("Fix")
         
 
 
 def main():
     # Main Function
-    number = int(input("Enter a number: "))
+    number = input("Enter a number: ")
 
     try:
-        isnumeric(number)
+        number = number.isalnum()
     except NotNumericError as e:
         print(f"Error: {e}")
     else:
