486904947e1e856f32cc6d0f1ba75af15f4c6f83
wstandley <williamstandley03@gmail.com>
2024-11-20 14:32:48 -0600

    Assignment PE2 Module 3.6

--- custom_error.py
+++ custom_error.py
@@ -8,29 +8,34 @@
 class NotNumericError(Exception):
     def __init__(self, number, message = "That is not a number"):
         self.number = number
-        self.message = message
-        super().__init__(self.message)
+        self.message = message 
+        super().__init__(self.message) # Sets super class
 
     def __str__(self):
+        # Prints message when error is caught
         return f"{self.number} -> {self.message}"
     
-    def isnumeric(number):
-        if number == False:
-            raise NotNumericError(number)
-        print("Fix")
+def isnumeric(number):
+    # Raises Error if number is not all numbers
+    if not number.isnumeric():
+        raise NotNumericError(number) 
         
 
 
 def main():
     # Main Function
+    # User Input
     number = input("Enter a number: ")
-
+    
     try:
-        number = number.isalnum()
+        # Runs isnumeric() function
+        isnumeric(number)
+    # Catches NotNumeric Error
     except NotNumericError as e:
         print(f"Error: {e}")
     else:
-        print("Good Job!")
+        # Only prints if execution runs without error
+        print("You Entered a Number!")
     finally:
         print("Game Over")
 
